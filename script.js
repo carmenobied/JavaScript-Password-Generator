a5ca1c3b6b0701cac66ed0c326486cfa5aa0f0ff
Carmen Obied <carmen.obied@gmail.com>
2020-04-13 19:36:01 -0700

    added key relevant generator functions to script.js file

--- script.js
+++ script.js
@@ -11,4 +11,27 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// starting with the generator functions below
+
+document.getElementById(“password”)
+
+generateBtn.addEventListener("generate", writePassword);
+
+function getRandomLower() {
+
+    return(String.fromCharCode()}
+    
+    console.log(Math.random() * 10)
+
+
+// for (var i = 0; i < 10; i++) {
+//     // Generate a random number between 1 and 10
+//     // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
+//     var num = Math.floor(Math.random() * 10) + 1;
+
+//     // Display in console
+// //     console.log(num);
+//   }
+
+// passwordLength = parseInt(prompt("Please select a password length between 8 and 128 characters."));
