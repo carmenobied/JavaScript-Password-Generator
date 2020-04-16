55edc59bb0ecc5fe4008d88118b847a11c7d0888
Carmen Obied <carmen.obied@gmail.com>
2020-04-15 20:19:18 -0700

    changed input variables and added values to these properties, added writePassword function, generateButton, passwordLength, generatePassword, tried to improve the if else and for loops (still needs fixing), return statements, and tried to improve the syntax and logic

--- script.js
+++ script.js
@@ -4,62 +4,76 @@ var generateBtn = document.querySelector("#generate");
 // Welcome user and prompt user to generate a password
 window.onload = alert("Welcome! Please click 'Generate password' to start!");
 
-// User input variables: 
-var enter;
-var Num;
-var SpecialChar;
-var Uppercase;
-var Lowercase;
+// // User input variables (strings): 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numeric = "0123456789";
+var specialChar = "!$^*&#-=+_?@$%^()";
+
+var generateButton = document.getElementById('btn');
 
 // Event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateButton.addEventListener('click', writePassword);
 
 // WHEN user clicks button to generate password: THEN user is presented with series of prompts for password criteria
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// Function to generate password
+  passwordText.value = password;
+}
+
+// Function to generate password + // Asks for user input
+// When prompted for length of password, then user chooses length of at least 8 characters and no more than 128 characters
+// passwordLength = parseInt(prompt("Please select a password length between 8 and 128 characters."));
+
 function generatePassword() {
+  var passwordLength = parseInt(prompt("How many characters would you like in your password? Choose between 8 and 128"));
+  
+// First validate user input.Then continue user input prompts.
 
-// Asks for user input
-  enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+for (i=0; i<passwordLength;i++) {
 
-// WHEN prompted for password criteria: THEN user selects which criteria to include in password
-
-// First validate user input.
-  if (!enter) {
+  if (!passwordLength) {
     alert("This password needs a value");
     } 
 
-// When prompted for length of password, then user chooses length of at least 8 characters and no more than 128 characters
-// passwordLength = parseInt(prompt("Please select a password length between 8 and 128 characters."));
-  else if (enter < 8 || enter > 128) {
-    enter = parseInt(prompt("You must select a password length between 8 and 128 characters."));
+  else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt("You must select a password length between 8 and 128 characters."));
     } 
+
   else {
+    lowerCase = confirm("Do you want this password to contain lowercases?");
+    upperCase = confirm("Do you want this password to contain uppercases?");
+    numeric = confirm("Do you want this password to contain numbers?");
+    specialChar = confirm("Do you want this password to contain special characters?");
 
-    // Once user input is validated, continue user input prompts
-    Num = confirm("Will this password contain numbers?");
-    SpecialChar = confirm("Will this password contain special characters?");
-    Uppercase = confirm("Will this password contain Uppercase letters?");
-    Lowercase = confirm("Will this password containLowercase letters?");
-      };
+// WHEN prompted for password criteria: THEN user selects which criteria to include in password
 
-// WHEN user is prompted for character types to include in password: THEN user chooses lowercase, uppercase, numeric, and/or special characters
-      // Lower case 
+// WHEN user is prompted for character types to include in password, THEN user chooses lowercase, uppercase, numeric, and/or special characters
+      // Lowercase 
       function getRandomLower() {
         return(String.fromCharCode((Math.floor(Math.random() * 26) + 97); 
       }
 
-      // Upper case 
+      // Uppercase 
       function getRandomUpper() {
           return(String.fromCharCode((Math.floor(Math.random() * 26) + 97); 
       }
 
-      // Upper case 
+      for (var i = 0; i < input.length; i++) {
+        if (input[i] === input[i].toUpperCase()) { 
+          return true;
+        }
+      }
+    };
+
+      // Numeric
       function getRandomNumber()) {
         return(String.fromCharCode((Math.floor(Math.random() * 10) + 48); 
       }
           
-      // Upper case 
+      // Special character
       function getRandomSymbol() {
         const symbols = !@#%^&*{}[],.
         return symbols [Math.floor(Math.random() * symbols.length]; 
@@ -73,8 +87,8 @@ function generatePassword() {
   push() 
 
 // WHEN password is generated: THEN password is either displayed in an alert or written to the page.
-  document.getElementById("password");
-  alert("This password needs a value");
+
+
 
 // // Notes: 
 // //  for (var i = 0; i < x; i++) {
